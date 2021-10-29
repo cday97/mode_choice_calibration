b81590ffd7b3dc26f46d439c1818f352f3f7f1a2
Christopher Day <christophersday@gmail.com>
2021-10-29 17:21:46 -0600

    Update targets to build mnl and path tables and plots

--- _targets.R
+++ _targets.R
@@ -13,26 +13,23 @@ tar_option_set(packages = c("tidyverse", "bookdown"))
 # Define custom functions and other global objects.
 # This is where you write source(\"R/functions.R\")
 # if you keep your functions in external scripts.
-source("R/functions.R")
-
+source("R/datamaker.R")
 
 # Targets necessary to build your data and run your model
 data_targets <- list(
-  tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-  tar_target(summary, summ(data)) # Call your custom functions as needed.
+  tar_target(mnldata, read_mc_data("data/modeChoice-slc-mnl.csv")),
+  tar_target(mnlgraph, build_barchart(mnldata, "MNL")),
+  tar_target(pathdata, read_mc_data("data/modeChoice-slc-path.csv")),
+  tar_target(pathgarph, build_barchart(pathdata, "Path"))
 )
-
-
 
 # Targets necessary to build the book / article
-book_targets <- list(
-  tar_target(report, rmarkdown::)
-)
+#book_targets <- list(
+#  tar_target(report, rmarkdown::)
+#)
 
-
-
-# run all targets
+# End this file with a list of target objects.
 list(
-  data_targets, 
-  book_targets
+  data_targets
+  #book_targets
 )
